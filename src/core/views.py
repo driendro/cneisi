ee1960ef3b6366160397d370b3a38d8d359766aa
Fermat <jorge.ronconi@gmail.com>
2024-09-27 04:14:12 +0200

    Modificacion del LandingPage (#3)
    * nuevo_home
    
    * Se actulizaron los paceholder en el home nuevo y se borro el viejo home
    
    * En el landingPAge se renderizan las actividades por fecha, mostrando titulo, portada y orador
    
    * Se añadieron las fuentes al home
    
    * finalizacion de la landignpage, y creacion de un modelo para los auspisiantes
    
    * se añadieron las imagenes del cronograma

--- src/core/views.py
+++ src/core/views.py
@@ -13,9 +13,10 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.list import ListView
 from tablib import Dataset
 import threading
+from datetime import date
 
 # Create your views here.
-from .models import UserAsistente, UserCoordinador, Actividad, Dependencia
+from .models import UserAsistente, UserCoordinador, Actividad, Dependencia, Sponsors
 from .forms import UsuariosForm, AsistenteUpdateForm,generar_cadena_alternante
 from .mixins import GroupRequiredMixin
 from .resources import AsistenteResource, CoordinadorResource
@@ -56,6 +57,22 @@ class PerfilHome(LoginRequiredMixin, View):
                 return redirect('home')
         else:
             return redirect('home')
+
+class LandingPage(TemplateView):
+    model = Actividad
+    template_name = 'landing/home.html'
+    context_object_name = 'actividades'
+    
+    def get_context_data(self, **kwargs):
+        # En el Contexto se van a renderizar segun la fecha de la actividad, para poder mostrarlas mas facil en el home
+        context = super().get_context_data(**kwargs)
+        actividades = Actividad.objects.filter(inscripcion=True).all()
+        context['actividades'] = actividades
+        context['actividad_25'] = actividades.filter(fecha=date(2024, 10, 25)).all()
+        context['actividad_26']= actividades.filter(fecha=date(2024,10,26)).all()
+        context['actividad_27']= actividades.filter(fecha=date(2024,10,27)).all()
+        context['sponsors'] = Sponsors.objects.all()
+        return context
 
 
 ###############################Coordinador########################################################################################################################

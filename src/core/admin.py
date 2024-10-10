0391a93ae782eb569034c9dd77f1909edf87c462
Fermat <jorge.ronconi@gmail.com>
2024-10-10 15:45:23 -0300

    se añadieron filtros en la vista de admin de los asistnetes

--- src/core/admin.py
+++ src/core/admin.py
@@ -18,9 +18,20 @@ class ActividadAdmin(admin.ModelAdmin):
             },
         ),
     ]
+    
+class UserAsistenteAdmin(admin.ModelAdmin):
+    # Lista los campos que se mostrarán en el listado del admin en formato tabla
+    list_display = ('user__last_name', 'user__first_name', 'user__email', 'documento', 'dependencia__nombre_corto')
+
+    # Opcional: Añade búsqueda por los campos que desees
+    search_fields = ('user__last_name', 'user__first_name', 'user__email', 'documento', 'dependencia__nombre_corto', 'dependencia__nombre_largo')
+
+    # Opcional: Filtrado por año u otro campo
+    list_filter = ('dependencia__nombre_corto','talle_ropa__designacion')
+
 
 admin.site.register(UserCoordinador)
-admin.site.register(UserAsistente)
+admin.site.register(UserAsistente, UserAsistenteAdmin)
 admin.site.register(TallesRemeras)
 admin.site.register(Dependencia)
 admin.site.register(Aula)
